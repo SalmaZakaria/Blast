e52b01a5f0f4aebdc174aa2b77b532c2e73820fc
Salma Zakaria <53764024+SalmaZakaria@users.noreply.github.com>
2021-01-27 09:54:33 +0200

    Update LoadingPAM.py

--- LoadingPAM.py
+++ LoadingPAM.py
@@ -1,24 +1,25 @@
 
 DictPAM = {}
-Proteins = ['G', 'A', 'V', 'L', 'I', 'P', 'S', 'T', 'D', 'E', 'N', 'Q', 'K', 'R', 'H', 'F', 'Y', 'W', 'M', 'C', 'B', 'Z', 'X']
+Proteins = ['A', 'R', 'N', 'D', 'C', 'Q', 'E', 'G', 'H', 'I', 'L', 'K', 'M', 'F', 'P', 'S', 'T', 'W', 'Y', 'V', 'B', 'J', 'Z', 'X']
 
-def PAMMatrix():
-    with open("PAM.txt", 'r') as file:
+def BlosumMatrix():
+    with open("Blosum.txt", 'r') as file:
         lines = file.read().splitlines()
-        PAM = []
+        Blosum = []
         for line in lines:
-            PAMList = [int(i) for i in line.split()] 
-            PAM.append(PAMList)
-        return PAM
+            BlosumList = [int(i) for i in line.split()] 
+            Blosum.append(BlosumList)
+        return Blosum
+
 
 def generateDic():
-    PAM_MATRIX = PAMMatrix()
+    Blosum_MATRIX = BlosumMatrix()
 
     for i in range(0, len(Proteins)):
         DictPAM[Proteins[i]] = {}
         Helper_Dict = {}
         for j in range(0, len(Proteins)):
-            Helper_Dict[Proteins[j]] = PAM_MATRIX[i][j]
+            Helper_Dict[Proteins[j]] = Blosum_MATRIX[i][j]
 
         DictPAM[Proteins[i]] = Helper_Dict
     
